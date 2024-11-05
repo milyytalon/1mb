5c83954a97f0a51133ed3433ef1fecba334df630
milyytalon <kamily.padovani@escola.pr.gov.br>
2024-11-05 11:42:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+<HTML>
+      <HEAD>
+      <TITLE> calculo  2 notas de uma média de um aluno
+      <HEAD>
+      <BODY>
+       <H1> calculo 2 notas de uma média de um aluno </H1>
+       <input type = 'number' max= '100' id= 'nota'_1'
+       placeholder
